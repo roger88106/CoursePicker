e93e8c4e9a799f54019db77dd93b6f920a49408d
roger <roger88106@gmail.com>
2023-04-15 18:27:29 +0800

    更新response

--- src/main/java/com/example/demo/vo/response/CoursesResponse.java
+++ src/main/java/com/example/demo/vo/response/CoursesResponse.java
@@ -1,5 +1,8 @@
 package com.example.demo.vo.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class CoursesResponse {
 
 }
--- src/main/java/com/example/demo/vo/response/StudentResponse.java
+++ src/main/java/com/example/demo/vo/response/StudentResponse.java
@@ -1,4 +1,7 @@
 package com.example.demo.vo.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class StudentResponse {
 }
